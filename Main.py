00a0ca1e42bd4ac7e64e4fb060b2a8c26a1b5afb
JacobNorlin <jacob.norlinandersson@gmail.com>
2016-11-20 17:53:07 +0100

    Removed debug prints

--- Main.py
+++ Main.py
@@ -1,16 +1,33 @@
 import data_reader
 import a_priori
-path = './data/T10I4D100K.dat'
-#path = './data/test.dat'
+import time
+import argparse
 
-transactions = [[n for n in t.strip().split(" ")] for t in data_reader.read_file_transaction_level(path)]
+#CLI Stuff
+parser = argparse.ArgumentParser(description='Find frequent item sets')
+parser.add_argument('fp', type=str, help="Path for data file", default="'./data/T10I4D100K.dat'")
+parser.add_argument('s', type=int, help="Minumun support threshold")
+parser.add_argument('c', type=float, help="Minumun confindence treshold", default=0.75)
 
+args = parser.parse_args()
+
+path = args.fp
+c = args.c
+s = args.s
+
+transactions = [[int(n) for n in t.strip().split(" ")] for t in data_reader.read_file_transaction_level(path)]
 print("Loaded data")
 
-counts_map = a_priori.apriori(transactions, 1000)
-rules = a_priori.generate_association_rules(counts_map, 0.75)
+start = time.time()
+
+counts_map = a_priori.apriori(transactions, s)
+rules = a_priori.generate_association_rules(counts_map, c)
+
+end = time.time()
 
 print("==Inferred rules==")
+
+print("Found "+str(len(rules)) +" rules in "+str((end-start))+" seconds")
 for rule in rules:
     a, b, confidence = rule
     print(str(a) + " -> " + str(b) + " : c: "+str(confidence))
--- a_priori.py
+++ a_priori.py
@@ -6,25 +6,29 @@ def count_items_in_transactions(transactions, prev_frequent_items, k, s):
     counts = defaultdict(int)
     frequent_items = set()
     for t in transactions:
+        #Find frequent items within the transaction, for L1 all items are frequent
         fi = t if len(prev_frequent_items) == 0 else [item for item in t if item in prev_frequent_items]
         candidates = set(itertools.combinations(fi, k))
+        #Count how many times each candidate occurs in transaction
         for candidate in candidates:
             candidate = tuple(sorted(candidate))
             counts[candidate] += 1
+            #If candidate occurs more than s times add it to frequent items
             if counts[candidate] >= s:
                 frequent_items |= set(candidate)
-    return counts, frequent_items
+    #Filter out all unfrequent items from count map and return counts and frequent items
+    return {k:v for k,v in counts.items() if v >= s}, frequent_items
 
 
 def apriori(transactions, t):
     counts, fi = count_items_in_transactions(transactions, [], 1, t)
-    print("First pass count done")
+    #print("First pass count done")
     stack = [fi]
     frequent_items = []
     counts_map = {1:counts}
     i = 2
     while len(stack) > 0:
-        print("starting L", i)
+        #print("starting L", i)
 
         prev_all_items = stack.pop()
 
@@ -44,7 +48,7 @@ def generate_association_rules(count_map, t):
         if k == 1: continue
         for item in k_sets:
             subsets = [(value, len(value)) for value in powerset(item)]
-            all_rules = [(subset, set(item) - set({subset} if i == 1 else set(subset)), count_map[k][item] / count_map[i][subset]) for subset, i in subsets]
+            all_rules = [(subset, set(item) - set((subset) if i == 1 else set(subset)), count_map[k][item] / count_map[i][subset]) for subset, i in subsets]
             rules += [(a, b, c) for a, b, c in all_rules if c >= t]
 
     return rules
